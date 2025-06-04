de9051a287f837c4506733a9c245a4b9c532d0fc
DaWebAlpha <tribillio@gmail.com>
2025-06-04 10:24:48 -0700

    NOT EQUAL

--- query.sql
+++ query.sql
@@ -12,5 +12,23 @@
 	Select the brand, model, condition and price from cars
 		where the condition equals 0
 */
+--SELECT brand,  model, condition, price FROM cars WHERE condition = 0;
 
-SELECT brand,  model, condition, price FROM cars WHERE condition = 0;
+
+/*
+	Select the brand, model, condition and price from cars
+		find results where the price is less than $50,000
+*/
+
+--SELECT brand, model, condition, price WHERE price < 50000;
+
+--SELECT * FROM cars WHERE year = 2001 and model = 'X-Type';
+
+/*
+	Filter out cars from 1965
+		Select the brand, model, year and price
+*/
+
+SELECT brand, model, year, price FROM cars WHERE year != 1965;
+
+
