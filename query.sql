b153c522af0a6584e5458f736284315c5f222975
DaWebAlpha <tribillio@gmail.com>
2025-06-04 10:53:12 -0700

    WILD Card

--- query.sql
+++ query.sql
@@ -29,7 +29,15 @@
 		Select the brand, model, year and price
 */
 
+
+--NOT EQUAL TO --
 --SELECT brand, model, year, price FROM cars WHERE year != 1965;
-SELECT brand, model, year, price FROM cars WHERE year <> 1965;
+--SELECT brand, model, year, price FROM cars WHERE year <> 1965;
 
 
+
+/*
+	Select the brand, model, color and year
+		find any car where the color includes 'green'
+*/
+SELECT brand, model, color, year FROM cars WHERE color LIKE '%green%';
