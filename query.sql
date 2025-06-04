c0e1366e9e237054f3b996d0342b80a8331a6779
DaWebAlpha <tribillio@gmail.com>
2025-06-04 07:26:21 -0700

    WHERE CONDITION

--- query.sql
+++ query.sql
@@ -2,4 +2,15 @@
 
 --SELECT * FROM cars
 
-SELECT brand, model, price FROM cars
+--SELECT brand, model, price FROM cars
+
+--SELECT brand, model, year, condition FROM cars;
+
+
+/* FILTERING RESULTS */
+/*
+	Select the brand, model, condition and price from cars
+		where the condition equals 0
+*/
+
+SELECT brand,  model, condition, price FROM cars WHERE condition = 0;
